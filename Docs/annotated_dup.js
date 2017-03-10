8a6b446bd896a546d98f6460c70c13ecfe1141ca
FuseBuildMonkey <buildmonkey@fusepowered.com>
2017-03-10 15:09:52 -0500

    Documentation release for 2.7.2.0

--- Docs/annotated_dup.js
+++ Docs/annotated_dup.js
@@ -1,4 +1,4 @@
-var annotated =
+var annotated_dup =
 [
     [ "FuseMisc", "namespace_fuse_misc.html", "namespace_fuse_misc" ],
     [ "FuseSDK", "class_fuse_s_d_k.html", "class_fuse_s_d_k" ]
---
+++ Docs/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgilmnprstuvz",
+  1: "cfiprv",
+  2: "f",
+  3: "acdefgilmprstuz",
+  4: "acefimprsv",
+  5: "afgi",
+  6: "binrsu",
+  7: "afginprstv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "events"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Events"
+};
+
